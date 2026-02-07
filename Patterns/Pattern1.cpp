b2c23472e153cfd31dc57a228ce9a0c215d56606
Mayank007-ahjin <mkarnatak23@gmail.com>
2026-02-07 23:34:48 +0530

    Adding pattern codes

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,39 @@
+//for normal case://
+#include <bits/stdc++.h>
+using namespace std;
+void print1(int n){
+    for (int i=0;i<n;i+=1){
+        for (int j=0;j<n;j+=1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+    return 0;
+}
+
+//for test case://
+#include <bits/stdc++.h>    
+using namespace std;
+void print1(int n){
+    for (int i=0;i<n;i+=1){
+        for (int j=0;j<n;j+=1){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    for (int i=0;i<t;i+=1){
+        int n;
+        cin>>n;
+        print1(n);
+    }
+    return 0;
+}
