88223ec7eaae6e4a8f9417cde793d9eadea62e50
Mayank007-ahjin <mkarnatak23@gmail.com>
2026-02-21 10:33:10 +0530

    updating

--- Patterns/Pattern12.cpp
+++ Patterns/Pattern12.cpp
@@ -28,3 +28,36 @@ int main(){
     Print1(n);
     return 0;
 }
+//for Test case
+#include <bits/stdc++.h>
+using namespace std;
+void Print1(int n){
+    int space=2*(n-1);
+    for (int i=1;i<=n;i++){
+        //numbers
+        for ( int j=1;j<=i;j++){
+            cout<<j;
+
+        }
+        //spaces
+        for(int j=0;j<=space;j++){
+            cout<<" ";
+        }
+        //numbers
+         for ( int j=i;j>=1;j--){
+            cout<<j;
+
+        }
+        cout<<endl;
+        space -=2;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    for (int i=0;i < t;i++){
+        int n;
+        cin>>n;
+        Print1(n);
+    }
+}
--- Patterns/Pattern19.cpp
+++ Patterns/Pattern19.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void Print1(int n) {
     int iniS = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
 
         // stars
         for (int j = 1; j <= n - i; j++) {
@@ -24,17 +24,32 @@ void Print1(int n) {
         iniS += 2;
         cout << endl;
     }
-}
+    int iniS2= 2 * n - 2;
+    for (int i = 1; i <= n; i++) {
 
-int main() {
-    int t;
-    cin >> t;
+        // stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
 
-    for (int i = 0; i < t; i++) {
-        int n;
-        cin >> n;
-        Print1(n);
+        // spaces
+        for (int j = 0; j <= iniS2; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <=i; j++) {
+            cout << "*";
+        }
+
+        iniS2 -= 2;
+        cout << endl;
     }
 
+}
+int main(){
+    int n;
+    cin>>n;
+    Print1(n);
     return 0;
 }
